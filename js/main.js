ba58731b7c7735e5e69c7f8fc6d2f8bf483e06dc
maro-hani <marohani22@yahoo.com>
2021-11-18 03:47:53 +0200

    Adding changeWebColor-feature and scrolling-feature

--- js/main.js
+++ js/main.js
@@ -1,23 +1,57 @@
- 
+/* === === === === -< Change BackgroundColor Of Nav And Show ScrollToTop Btn >- === === === === */
 
-/*============================= change backgroundColor of nav and show scroll to top btn */
- $(window).scroll(function () {
-    let scrollTo = $(window).scrollTop(); 
-     let aboutPosition = $("#about").offset().top;
-     if(scrollTo>aboutPosition-50)
-     {
-         $("nav").css("backgroundColor","rgba(0,0,0,.5)");
-         $(".btnUp").css("opacity","1");
-     }
-     else
-     {
-        $("nav").css("backgroundColor","transparent");
-        $(".btnUp").css("opacity","0");
-     }
- });
-// =========================== show btn
- $(".btnUp").click(()=>{
-    $("html,body").animate({scrollTop:0},1500);
- });
+const aboutPosition = $("#about").offset().top;
 
- 
+$(window).scroll(function () {
+
+    let scrollTo = $(window).scrollTop();
+
+    if (scrollTo > aboutPosition - 50) {
+        $("nav").css("backgroundColor", "rgba(0,0,0,.5)");
+        $(".btnUp").css("opacity", "1");
+    } else {
+        $("nav").css("backgroundColor", "transparent");
+        $(".btnUp").css("opacity", "0");
+    }
+});
+/* === === === === -< Show BtnUp >- === === === === */
+$(".btnUp").click(() => {
+    $("html,body").animate({
+        scrollTop: 0
+    }, 1500);
+});
+
+
+/* === === === === -< Scrolling To Position Of The Link Clicked On The Navbar >- === === === === */
+$("nav a[href ^='#']").click(function () {
+
+    let currentLink = $(this).attr("href");
+
+    $("html,body").animate({
+        scrollTop: $(currentLink).offset().top
+    }, 1500);
+
+});
+
+
+/* === === === === -< color Box toggle  >- === === === === */
+$(".toggleBtn").click(function () {
+
+    let colorBoxwidth = $(".contentBox").innerWidth();
+
+    if ($(".colorBox").css("left") == "0px") {
+        $(".colorBox").animate({
+            left: -colorBoxwidth + 20
+        }, 500);
+    } else {
+        $(".colorBox").animate({
+            left: "0px"
+        }, 500);
+    }
+});
+
+/* === === === === -< change web color >- === === === === */
+$(".colorBox .color-item").click(function () {
+    let bgColor = $(this).css("backgroundColor");
+    $(".change-color").css("color", bgColor);
+});
